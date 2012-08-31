852000914637cd253929d79218403bfd9d90a912
f.mithieux <f.mithieux@SANTEVET-RTUYIO.santevet.lan>
2012-08-31 11:14:17 +0200

    - Add Comments

--- gestion/app.php
+++ gestion/app.php
@@ -1,4 +1,17 @@
 <?php
+/*
+ * ArgetFwk - 2012
+ * -------------------
+ * FrameWork MVC en phase de développement et d'amélioration
+ * 
+ * 
+ * Utilisation des FrameWorks :
+ *  - TwitterBootstrap 2.1
+ *  - Doctrine 2
+ *  - Twig
+ * 
+ * -------------------
+ */
 session_start();
 require 'app/autoload.php';
 ?>
