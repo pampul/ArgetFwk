46566cb32b2140ff2c9453475331bf383614b482
Argetloum <florian.mithieux@gmail.com>
2013-01-07 14:38:45 +0100

    - Correction de doc

--- app/ajax.php
+++ app/ajax.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
- * 2012 ArgetFwk
- * 
- * FrameWork MVC en phase de développement et d'amélioration
- * 
- * Utilisation des FrameWorks :
- *  - TwitterBootstrap 2.1
- *  - Doctrine 2
- *  - Twig
- * 
- * -------------------
- * 
- *
- * @author Florian MITHIEUX <florian.mithieux@gmail.com>
- * @version  0.51a
- */
 session_start();
 
 if (isset($_POST['method']) && isset($_POST['controller'])) {
