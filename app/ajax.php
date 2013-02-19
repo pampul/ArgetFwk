22c72577145fbaa78da4724c9b7b6b870fafb273
Argetloum <florian.mithieux@gmail.com>
2013-02-19 11:55:17 +0100

    - Correction bug backoffice ajax

--- app/ajax.php
+++ app/ajax.php
@@ -6,7 +6,7 @@ if (isset($_POST['method']) && isset($_POST['controller'])) {
 
     define('BACKOFFICE_ACTIVE', '');
     define('PATH_TO_IMPORTANT_FILES', '../');
-    define('PATH_TO_BACKOFFICE_FILES', 'gestion/');
+    define('PATH_TO_BACKOFFICE_FILES', '../gestion/');
     define('GET_METHOD', $_POST['method']);
     define('GET_CONTROLLER', $_POST['controller']);
 
